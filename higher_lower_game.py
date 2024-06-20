081f121aebd03762274ccfa5d02ac18357b45149
dwayne-99 <dwaynephillips01@gmail.com>
2024-06-20 12:21:39 -0400

    .

---
+++ higher_lower_game.py
@@ -0,0 +1,54 @@
+from game_data import data
+import random
+import os
+
+def clear_screen():
+  if os.name == 'nt':
+    os.system('cls')
+  else:
+    os.system('clear')
+
+def format_data(account):
+  account_name = account["name"]
+  account_description = account["description"]
+  account_country = account["country"]
+  return f"{account_name}, a {account_description}, from {account_country}"
+
+def check_answer(guess, a_followers, b_followers):
+  if a_followers > b_followers:
+    return guess == "a" # returns True
+  else:
+    return guess == "b" # returns False
+
+score = 0
+game_should_continue = True
+account_b = random.choice(data)
+
+while game_should_continue:
+
+  account_a = account_b
+  account_b = random.choice(data)
+  
+  while account_a == account_b:
+    account_b = random.choice(data)
+  
+  print(f"Compare A: {format_data(account_a)}.")
+  print("")
+  print(f"Against B: {format_data(account_b)}.")
+  print("")
+  guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+  
+  a_follower_count = account_a["follower_count"]
+  b_follower_count = account_b["follower_count"]
+  is_correct = check_answer(guess, a_follower_count, b_follower_count)
+  clear_screen()
+  
+  if is_correct:
+    score += 1
+    print("")
+    print(f"You're right! Current score: {score}")
+    print("")
+  else:
+    game_should_continue = False
+    print("")
+    print(f"Sorry thats wrong. Final score: {score}")
